a5f67e01fe1fdec2c86cb7c3e2cebe6a47f2ef81
daniherresan <danielherresan@gmail.com>
2020-02-19 12:11:33 -0500

    hola mundo

---
+++ clases/clase1/holamundo.py
@@ -0,0 +1,12 @@
+#Codigo primera clase
+nombre = "Daniel"
+print (nombre)
+print (type(nombre))
+edad = 26
+print (edad)
+print (type(edad))
+dinero = 418943348273927394.34
+print (dinero)
+print (type(dinero))
+
+
